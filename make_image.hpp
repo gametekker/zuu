26c1a2cbcf807c7157a06cbcd29c6964e76ab433
gametekker <104738069+gametekker@users.noreply.github.com>
2024-07-15 02:00:11 -0600

    Delete make_image.hpp

--- make_image.hpp
+++
@@ -1,3 +0,0 @@
-#include <vector>
-#include <complex>
-std::vector<std::vector<std::complex<double>>> make_image(std::pair<double,double> xlim, std::pair<double,double> ylim, int res, std::complex<double> (*func)(const std::complex<double>&));
